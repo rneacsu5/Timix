d9904fa44dca29750fddb448304254cd05628f2b
OriginalNexus <razvan.neacsu5@gmail.com>
2014-07-12 22:06:38 +0300

    Colorful room

--- test.c
+++ test.c
@@ -7,10 +7,9 @@
 int a = 0;
 float r;
 float loc[] = {5, 2.5, 5, 1};
-float color[] = {1, 1, 1, 1};
+float white_color[] = {0.5, 0.5, 0.5, 1};
 
-
-void display(void) 
+void display(void)
 {
 	// Clear the color buffer, restore the background
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -18,138 +17,142 @@ void display(void)
 	glLoadIdentity();
 	// Set up the camera
 	gluLookAt(5, 1.7, 5, 5 + sin(r), 1.7, 5 + cos(r), 0, 1, 0);
-
+	// Set camera position
 	glLightfv(GL_LIGHT0, GL_POSITION, loc);
-	// Set the drawing color to white
+	// Set the drawing color to cyan
 	glColor3f(0, 1, 1);
-	// Draw a wireframe teapot of size 5
+
 	glPushMatrix();
-		glTranslatef(2, 0.1, 2);
-		glRotatef(a, 0, 1, 0);
-		glutSolidTeapot(0.2);
+	glTranslatef(2, 0.15, 2);
+	glRotatef(a, 0, 1, 0);
+	glutSolidTeapot(0.3);
 	glPopMatrix();
 
+	int i, j;
+
 	//Floor
+	glColor3f(0.7, 1, 0.7);
 	glBegin(GL_QUADS);
-		int i, j;
-		for (i = 0; i < 5; i++) {
-			for (j = 0; j < 5; j++) {
-				glVertex3f(2 * i, 0, 2 * j);
-				// glTexCoord2f(i, j);
-				glNormal3f(0, 1, 0);
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 10; j++) {
+			glVertex3f(i, 0, j);
+			// glTexCoord2f(i, j);
+			glNormal3f(0, 1, 0);
 
-				glVertex3f(2 * i, 0, 2 * j + 2);
-				// glTexCoord2f(i, j + 1);
-				glNormal3f(0, 1, 0);
+			glVertex3f(i, 0, j + 1);
+			// glTexCoord2f(i, j + 1);
+			glNormal3f(0, 1, 0);
 
-				glVertex3f(2 * i + 2, 0, 2 * j + 2);
-				// glTexCoord2f(i + 1, j + 1);
-				glNormal3f(0, 1, 0);
+			glVertex3f(i + 1, 0, j + 1);
+			// glTexCoord2f(i + 1, j + 1);
+			glNormal3f(0, 1, 0);
 
-				glVertex3f(2 * i + 2, 0, 2 * j);
-				// glTexCoord2f(i + 1, j);
-				glNormal3f(0, 1, 0);
+			glVertex3f(i + 1, 0, j);
+			// glTexCoord2f(i + 1, j);
+			glNormal3f(0, 1, 0);
 
-			}
 		}
+	}
 	glEnd();
 
-	float k;
-
 	//Wall 1
+	glColor3f(1, 1, 0);
 	glBegin(GL_QUADS);
-		for (i = 0; i < 5; i++) {
-			for (k = 0; k < 1.25; k += 0.25) {
-				glVertex3f(2 * i, 2 * k, 0);
-				// glTexCoord2f(i, k);
-				glNormal3f(0, 0, 1);
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 10; j ++) {
+			glVertex3f(i, 0.25 * j, 0);
+			// glTexCoord2f(i, k);
+			glNormal3f(0, 0, 1);
 
-				glVertex3f(2 * i, 2 * k + 2, 0);
-				// glTexCoord2f(i, k + 1);
-				glNormal3f(0, 0, 1);
+			glVertex3f(i, 0.25 * (j + 1), 0);
+			// glTexCoord2f(i, k + 1);
+			glNormal3f(0, 0, 1);
 
-				glVertex3f(2 * i + 2, 2 * k + 2, 0);
-				// glTexCoord2f(i + 1, k + 1);
-				glNormal3f(0, 0, 1);
+			glVertex3f(i + 1, 0.25 * (j + 1), 0);
+			// glTexCoord2f(i + 1, k + 1);
+			glNormal3f(0, 0, 1);
 
-				glVertex3f(2 * i + 2, 2 * k, 0);
-				// glTexCoord2f(i + 1, k);
-				glNormal3f(0, 0, 1);
+			glVertex3f(i + 1, 0.25 * j, 0);
+			// glTexCoord2f(i + 1, k);
+			glNormal3f(0, 0, 1);
 
-			}
 		}
+	}
 	glEnd();
 
 	//Wall 2
+	glColor3f(1, 0, 1);
 	glBegin(GL_QUADS);
-		for (i = 0; i < 5; i++) {
-			for (k = 0; k < 1.25; k += 0.25) {
-				glVertex3f(2 * i, 2 * k, 10);
-				// glTexCoord2f(i, k);
-				glNormal3f(0, 0, -1);
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 10; j ++) {
+			glVertex3f(i, 0.25 * j, 10);
+			// glTexCoord2f(i, k);
+			glNormal3f(0, 0, -1);
 
-				glVertex3f(2 * i, 2 * k + 2, 10);
-				// glTexCoord2f(i, k + 1);
-				glNormal3f(0, 0, -1);
+			glVertex3f(i, 0.25 * (j + 1), 10);
+			// glTexCoord2f(i, k + 1);
+			glNormal3f(0, 0, -1);
 
-				glVertex3f(2 * i + 2, 2 * k + 2, 10);
-				// glTexCoord2f(i + 1, k + 1);
-				glNormal3f(0, 0, -1);
+			glVertex3f(i + 1, 0.25 * (j + 1), 10);
+			// glTexCoord2f(i + 1, k + 1);
+			glNormal3f(0, 0, -1);
 
-				glVertex3f(2 * i + 2, 2 * k, 10);
-				// glTexCoord2f(i + 1, k);
-				glNormal3f(0, 0, -1);
+			glVertex3f(i + 1, 0.25 * j, 10);
+			// glTexCoord2f(i + 1, k);
+			glNormal3f(0, 0, -1);
 
-			}
 		}
+	}
 	glEnd();
 
 	//Wall 3
+	glColor3f(0, 1, 1);
 	glBegin(GL_QUADS);
-		for (i = 0; i < 5; i++) {
-			for (k = 0; k < 1.25; k += 0.25) {
-				glVertex3f(0, 2 * k, 2 * i);
-				// glTexCoord2f(i, k);
-				glNormal3f(1, 0, 0);
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 10; j ++) {
+			glVertex3f(0, 0.25 * j, i);
+			// glTexCoord2f(i, k);
+			glNormal3f(1, 0, 0);
 
-				glVertex3f(0, 2 * k + 2, 2 * i);
-				// glTexCoord2f(i, k + 1);
-				glNormal3f(1, 0, 0);
+			glVertex3f(0, 0.25 * (j + 1), i);
+			// glTexCoord2f(i, k + 1);
+			glNormal3f(1, 0, 0);
 
-				glVertex3f(0, 2 * k + 2, 2 * i + 2);
-				// glTexCoord2f(i + 1, k + 1);
-				glNormal3f(1, 0, 0);
+			glVertex3f(0, 0.25 * (j + 1), i + 1);
+			// glTexCoord2f(i + 1, k + 1);
+			glNormal3f(1, 0, 0);
 
-				glVertex3f(0, 2 * k, 2 * i + 2);
-				// glTexCoord2f(i + 1, k);
-				glNormal3f(1, 0, 0);
+			glVertex3f(0, 0.25 * j, i + 1);
+			// glTexCoord2f(i + 1, k);
+			glNormal3f(1, 0, 0);
 
-			}
 		}
+	}
 	glEnd();
 
-	//Wall 3
+	//Wall 4
+	glColor3f(1, 1, 1);
 	glBegin(GL_QUADS);
-		for (i = 0; i < 5; i++) {
-			for (k = 0; k < 1.25; k += 0.25) {
-				glVertex3f(10, 2 * k, 2 * i);
-				// glTexCoord2f(i, k);
-				glNormal3f(-1, 0, 0);
+	for (i = 0; i < 10; i++) {
+		for (j = 0; j < 10; j ++) {
+			glVertex3f(10, 0.25 * j, i);
+			// glTexCoord2f(i, k);
+			glNormal3f(-1, 0, 0);
 
-				glVertex3f(10, 2 * k + 2, 2 * i);
-				// glTexCoord2f(i, k + 1);
-				glNormal3f(-1, 0, 0);
+			glVertex3f(10, 0.25 * (j + 1), i);
+			// glTexCoord2f(i, k + 1);
+			glNormal3f(-1, 0, 0);
 
-				glVertex3f(10, 2 * k + 2, 2 * i + 2);
-				// glTexCoord2f(i + 1, k + 1);
-				glNormal3f(-1, 0, 0);
+			glVertex3f(10, 0.25 * (j + 1), i + 1);
+			// glTexCoord2f(i + 1, k + 1);
+			glNormal3f(-1, 0, 0);
 
-				glVertex3f(10, 2 * k, 2 * i + 2);
-				// glTexCoord2f(i + 1, k);
-				glNormal3f(-1, 0, 0);
+			glVertex3f(10, 0.25 * j, i + 1);
+			// glTexCoord2f(i + 1, k);
+			glNormal3f(-1, 0, 0);
 
-			}
 		}
+	}
 	glEnd();
 	// Swap buffers in GPU
 	glutSwapBuffers();
@@ -164,7 +167,7 @@ void reshape(int width, int height)
 	// Clear all the transformations on the projection matrix
 	glLoadIdentity();
 	// Set the perspective according to the window size
-	gluPerspective(80, (GLdouble) width / (GLdouble) height, 0.1, 60000);
+	gluPerspective(70, (GLdouble) width / (GLdouble) height, 0.1, 60000);
 	// Load back the modelview matrix
 	glMatrixMode(GL_MODELVIEW);
 }
@@ -175,15 +178,16 @@ void initialize(void)
 	glClearColor(0, 0, 0, 1);
 	glEnable(GL_DEPTH_TEST);
 	glEnable(GL_COLOR_MATERIAL);
-	glLightfv(GL_LIGHT0, GL_DIFFUSE, color);
-	glLightfv(GL_LIGHT0, GL_SPECULAR, color);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, white_color);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, white_color);
 	glEnable(GL_LIGHTING);
 	glEnable(GL_LIGHT0);
+	glShadeModel(GL_SMOOTH);
 }
 void tick(int value)
 {
 	a++;
-	if (a == 360) 
+	if (a == 360)
 		a = 0;
 	r = a * PI / 180;
 	glutPostRedisplay();
@@ -196,7 +200,7 @@ int main(int argc, char *argv[])
 	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
 	glutInitWindowSize(800, 600);
 	glutCreateWindow("Game");
-	glutDisplayFunc(display); 
+	glutDisplayFunc(display);
 	glutReshapeFunc(reshape);
 	initialize();
 	glutTimerFunc(10, tick, 0);
